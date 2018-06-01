9b3d2d07a6a85e5518ccb792504e4fac3193c094
ehron <ehron@teamgwinn.com>
2018-06-01 00:41:35 -0400

    get srv and record

--- config/resolveSrv.js
+++ config/resolveSrv.js
@@ -1,12 +1,19 @@
+const dns = require('dns');
+
 function resolveDB(addr, port) {
   return new Promise( function(resolve, reject) {
     if (isConsulAddr(addr)) {
+      var db_port
       dns.setServers(['127.0.0.1:8600'])
-      dns.resolveSrv(addr, (err, srv) => {
+      dns.resolveSrv(addr, function(err, srv) {
         if (err) {
           reject(err);
         } else {
-          resolve(buildDSN(srv[0].name, srv[0].port))
+          db_port = srv[0].port
+          dns.resolve4(addr, function (err, rec) {
+            resolve(buildDSN(rec[0], db_port))
+          })
+
         }
       })
     } else {
