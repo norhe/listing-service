76e47e57d5de2222bb99dc0a44089581c7fadcd6
ehron <ehron@teamgwinn.com>
2018-06-01 00:25:44 -0400

    more

--- config/resolveSrv.js
+++ config/resolveSrv.js
@@ -1,9 +1,6 @@
 function resolveDB(addr, port) {
   return new Promise( function(resolve, reject) {
-    if ((!addr.endsWith('service.consul')) ||
-        (!addr.endsWith('query.consul'))) {
-      resolve(buildDSN(addr, port))
-    } else {
+    if (isConsulAddr(addr)) {
       dns.setServers(['127.0.0.1:8600'])
       dns.resolveSrv(addr, (err, srv) => {
         if (err) {
@@ -12,12 +9,18 @@ function resolveDB(addr, port) {
           resolve(buildDSN(srv[0].name, srv[0].port))
         }
       })
+    } else {
+      resolve(buildDSN(addr, port))
     }
   })
 }
 
 function buildDSN(addr, port) {
   return 'mongodb://'+ addr + ':' + port
+}
+
+function isConsulAddr(addr) {
+  return addr.endsWith('consul')
 }
 
 module.exports = resolveDB
