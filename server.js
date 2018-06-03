5ed9244c9cd2d1cdf46555121c2e3dc5ef06649f
ehron <ehron@teamgwinn.com>
2018-06-03 10:24:32 -0400

    fix syntax

--- server.js
+++ server.js
@@ -14,16 +14,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // require('./config/resolveSrv')(db.db_addr, db.db_port).then( (dsn) => {
 var db_proxy_addr = 'mongodb://localhost:8001'
 MongoClient.connect(db_proxy_addr, { useNewUrlParser: true },(err, database) => {
-    if (err) return console.log(err)
+  if (err) return console.log(err)
 
-    // Make sure you add the database name and not the collection name
-    // TODO: load this from a file or env (consul)
-    dbConn = database.db(db.db_name)
-    require('./app/routes')(app, dbConn, {collection: db.db_col});
-    app.listen(PORT, () => {
-      console.log('We are live on ' + PORT);
-    });
-  })
-}).catch( (err) => {
-  console.log("An error occurred resolving the DSN" + err)
+  // Make sure you add the database name and not the collection name
+  // TODO: load this from a file or env (consul)
+  dbConn = database.db(db.db_name)
+  require('./app/routes')(app, dbConn, {collection: db.db_col});
+  app.listen(PORT, () => {
+    console.log('We are live on ' + PORT);
+  });
 })
+/*}).catch( (err) => {
+  console.log("An error occurred resolving the DSN" + err)
+})*/
