7f30cca334a8e3c8c1a9c263c6b458b8e1991c96
Ehron Gwinn <ehron@local>
2018-11-22 08:15:23 -0500

    create install script and update README

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-## Ignore
+# Listing Service
+
+This is a simple Node app for use as part of a broader demo.
+
+## Usage
+
+Clone the repo to an Ubuntu server, and run the install script.
+
+```
+cd install && sudo bash install.sh
+```
+
---
+++ install/install.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+# install Node
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# install the listing service app
+mkdir /home/ubuntu/src
+cd /home/ubuntu/src
+git clone https://github.com/norhe/listing-service.git
+cd listing-service
+npm install
+
+# systemd
+cat <<EOF | sudo tee /lib/systemd/system/listing.service
+[Unit]
+Description=server.js - Listing service API
+Documentation=https://example.com
+After=network.target
+
+[Service]
+Type=simple
+User=ubuntu
+ExecStart=/usr/local/bin/envconsul -prefix listing_conf /usr/bin/node /home/ubuntu/src/listing-service/server.js
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable listing.service
+sudo systemctl start listing.service
